d48063f07ab54a7da046feb870e64f7e21fd7f20
Наталья Крашенинникова <gr431_krnvi@lab-56-2.tomtit-tomsk.ru>
2024-11-12 15:52:32 +0700

    free

--- kisa/urls.py
+++ kisa/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import index
 from .views import other_page
-app_name = 'kisa'
+from .views import BBLoginView
+from .views import profile
+
+app_name = 'main'
 urlpatterns = [
-path('', index, name='index')
+    path('', index, name='index'),
+    path('<str:page>/', other_page, name='other'),
+path('account/login', BBLoginView.as_view(),name='login'),
+path('account/profile/', profile, name='profile'),
+
 ]
-
-
-path('<str:page>/', other_page, name='other')
-
