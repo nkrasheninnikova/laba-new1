95b239d1a160d533d1d10c99ad566afb768dee2e
Наталья Крашенинникова <gr431_krnvi@lab-56-2.tomtit-tomsk.ru>
2024-11-12 14:40:53 +0700

    first

---
+++ kisa/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class KisaConfig(AppConfig):
+    verbose_name = 'Электронная доска объявлений'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kisa'
